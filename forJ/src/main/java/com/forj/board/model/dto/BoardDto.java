fb6a131fa1ce4b74eb7620d99bf5f818ef55280b
zosunny <zoheyy108@naver.com>
2023-05-02 16:39:59 +0900

    feat: 게시판 CRUD 기능 완성

--- forJ/src/main/java/com/forj/board/model/dto/BoardDto.java
+++ forJ/src/main/java/com/forj/board/model/dto/BoardDto.java
@@ -2,4 +2,64 @@ package com.forj.board.model.dto;
 
 public class BoardDto {
 
+	private int boardId;
+	private String title;
+	private String writer;
+	private String writeDate;
+	private String content;
+	private int hit;
+//	private List<FileDto> files = new ArrayList<>(); 
+
+	BoardDto() {
+
+	}
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getWriteDate() {
+		return writeDate;
+	}
+
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
 }
