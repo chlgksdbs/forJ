f52a5123aad7e962398d43f77480e67bf5849472
zosunny <zoheyy108@naver.com>
2023-05-02 16:37:00 +0900

    feat: DBConfig 추가

---
+++ forJ/src/main/java/com/forj/config/DBConfig.java
@@ -0,0 +1,10 @@
+package com.forj.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.forj.*.model.repository")
+public class DBConfig {
+
+}
