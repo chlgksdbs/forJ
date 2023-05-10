4fa58123b450c32e6006ccd013ce9950c34ca131
chlgksdbs <chlgksdbs98@naver.com>
2023-05-10 17:54:11 +0900

    feat: JWT 로그인 구현

---
+++ server/src/main/java/com/forj/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.forj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.forj.jwt.JwtAuthenticationFilter;
+import com.forj.jwt.JwtTokenProvider;
+
+@Configuration
+@EnableWebSecurity // Web 보안 활성화를 위한 annotation
+public class WebSecurityConfig {
+	
+	private final JwtTokenProvider jwtTokenProvider;
+	
+	public WebSecurityConfig(JwtTokenProvider jwtTokenProvider) {
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
+	
+	// 출처 : https://datamoney.tistory.com/334
+	// csrf(cross site request forgery) : 사이트간 위조 요청 -> 인증된 사용자의 토큰을 탈취해 위조된 요청을 보냈을 경우 파악해 방지하는 것
+	// (1) .csrf().disable() : rest api에서는 권한이 필요한 요청 위해서 인증 정보를 포함시켜야 함. 서버에 인증 정보를 저장하지 않기 때문에 작성할 필요 없음 (JWT를 쿠키에 저장히자 않기 때문)
+	// (2) .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) : JWT를 사용하기 때문에 session도 사용하지 않음
+	// (3) .formLogin().disable() : HTTP Basic Authentication을 사용하지 않음
+	// (4) .httpBasic().disable() : Form Based Authentication을 사용하지 않음
+	// (5) .authorizeRequests() : 요청에 의한 보안 검사 시작
+	// (6) .antMatchers("/user").permitAll() : 해당 url 요청에 대해 모두 접근 가능하게 함
+	// (7) .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class) : UsernamePasswordAuthenticationFilter에 가기 전에 직접 작성한 JwtAuthenticationFilter를 실행함 
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.formLogin().disable()
+			.httpBasic().disable()
+			.authorizeRequests()
+			.antMatchers("/user").permitAll()
+			.and()
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
+	}
+}
--- server/src/main/java/com/forj/user/controller/UserController.java
+++ server/src/main/java/com/forj/user/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.forj.user.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.forj.jwt.JwtToken;
 import com.forj.user.model.service.UserService;
 
 @RestController
@@ -12,5 +17,12 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	
+	public ResponseEntity<JwtToken> loginSuccess(@RequestBody Map<String, String> loginForm) {
+		
+		JwtToken token = service.login(loginForm.get("username"), loginForm.get("password"));
+		
+		return ResponseEntity.ok(token);
+	}
 	
 }
