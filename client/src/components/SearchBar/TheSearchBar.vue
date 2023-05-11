38fd5de43c5a450074a2a633694ff005d1459cbc
chlgksdbs <chlgksdbs98@naver.com>
2023-05-12 00:59:16 +0900

    feat: SearchBar UI 생성

---
+++ client/src/components/SearchBar/TheSearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="TheSearchBar" align="right">
+        <div id="input_search_bar">
+            <input type="text" placeholder="검색어를 입력하세요." />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "TheSearchBar",
+}
+</script>
+<style scoped>
+input {
+    border: none;
+    margin: auto;
+    width: 160px;
+    height: 35px;
+}
+input:focus {
+    outline: none;
+}
+#input_search_bar {
+    width: 335px;
+    height: 41px;
+    border: 1px solid #dadada;
+    border-radius: 10px;
+}
+</style>
--- client/src/views/BoardPageView.vue
+++ client/src/views/BoardPageView.vue
@@ -1,17 +1,20 @@
 <template>
     <div id="BoardPageView">
         <the-heading></the-heading>
+        <the-search-bar></the-search-bar>
         <the-board-list></the-board-list>
     </div>
 </template>
 <script>
 import TheHeading from "@/components/Header/TheHeading.vue";
+import TheSearchBar from "@/components/SearchBar/TheSearchBar.vue";
 import TheBoardList from "@/components/BoardList/TheBoardList.vue";
 
 export default {
     name: "BoardPageView",
     components: {
         TheHeading,
+        TheSearchBar,
         TheBoardList,
     }
 }
