c22d435e3c45b39c6cc10b8983841f557b4680a3
chlgksdbs <chlgksdbs98@naver.com>
2023-05-11 16:39:11 +0900

    fix: merge 충돌 해결

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link to="/mypage">MyPage</router-link>
-    </nav>
-=======
->>>>>>> b1d47859236ac116c1a68c15af9f0e0a74b3af3e
     <router-view/>
   </div>
 </template>
@@ -19,18 +11,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
