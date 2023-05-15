9e8eb24082eb53e32e65ccad7a3a4a962cdc9ef6
zosunny <zoheyy108@naver.com>
2023-05-15 15:22:26 +0900

    feat: MyPageCart form 구현

---
+++ client/src/components/MyPage/MyPageCart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 class="modifyTitle">My 장바구니</h2>
+    <div class="modifyBox">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyPageCart',
+  components: {},
+  data() {
+    return {
+      message: '',
+    };
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
