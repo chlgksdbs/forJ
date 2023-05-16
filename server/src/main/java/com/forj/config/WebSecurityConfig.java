de2ab2db2c66ddeb5eab5d0c79424db162b7fc4d
chlgksdbs <chlgksdbs98@naver.com>
2023-05-16 13:19:33 +0900

    fix: 불필요한 파일 제거

--- server/src/main/java/com/forj/config/WebSecurityConfig.java
+++ server/src/main/java/com/forj/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.forj.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 //import com.forj.jwt.JwtAuthenticationFilter;
 //import com.forj.jwt.JwtTokenProvider;
--- server/src/main/java/com/forj/user/controller/UserController.java
+++ server/src/main/java/com/forj/user/controller/UserController.java
@@ -1,23 +1,18 @@
 package com.forj.user.controller;
 
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.mail.MessagingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.forj.jwt.JwtDto;
 import com.forj.jwt.JwtService;
 import com.forj.user.model.dto.UserDto;
 import com.forj.user.model.service.MailSendService;
