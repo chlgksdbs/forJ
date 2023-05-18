a081469495ae14a150f804e5338de18c1cfea575
chlgksdbs <chlgksdbs98@naver.com>
2023-05-19 03:39:09 +0900

    feat: vuex module 세팅 및 memberStore 생성

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+
+// module import
+import memberStore from "./modules/memberStore";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    memberStore,
+  },
+  plugins: [
+    createPersistedState({
+      // 브라우저 종료 시, 제거하기 위해 localStorage가 아닌 sessionStorage로 변경
+      // default: localStorage
+      storage: sessionStorage,
+    }),
+  ],
 })
---
+++ client/src/store/modules/memberStore.js
@@ -0,0 +1,19 @@
+import jwtDecode from "jwt-decode";
+import router from "@/router";
+
+const memberStore = {
+    state: {
+
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+};
+
+export default memberStore;
