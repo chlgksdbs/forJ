16b45b4fd9437daca3d3fdb336d1b28faa4e1ed5
chlgksdbs <chlgksdbs98@naver.com>
2023-05-24 17:22:35 +0900

    feat: 장바구니 아이템 UI 구현

---
+++ client/src/components/User/Item/UserPlanItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="UserPlanItem">
+    <div class="item_wrap">
+      <table class="item_table">
+        <colgroup>
+          <col style="width: 25%">
+          <col style="width: 75%">
+        </colgroup>
+        <tr class="item_title">
+          <td>여행 일정</td>
+          <td>{{ planItem.date }}</td>
+        </tr>
+        <tr>
+          <td>여행 장소</td>
+          <td>{{ planItem.location }}</td>
+        </tr>
+        <tr>
+          <td>여행 경비</td>
+          <td>{{ planItem.budget }}</td>
+        </tr>
+        <tr>
+          <td>여행 계획</td>
+          <td><button class="item_detail_btn">상세보기</button></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPlanItem',
+  components: {},
+  data() {
+    return {
+      message: '',
+    };
+  },
+  props: {
+    planItem: Object,
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.item_wrap {
+  border: 1px solid #d9d9d9;
+}
+.item_table {
+  width: 100%;
+  border-collapse: collapse;
+}
+.item_title {
+  background-color: #ddd;
+  font-weight: bold;
+}
+.item_table tr td {
+  border-bottom: 1px solid #d9d9d9;
+  padding: 10px;
+}
+.item_detail_btn {
+  cursor: pointer;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  padding: 3px 5px;
+}
+.item_detail_btn:hover {
+  background-color: #ddd;
+  color: #000;
+}
+#UserPlanItem {
+  margin: 15px 10px;
+}
+</style>
