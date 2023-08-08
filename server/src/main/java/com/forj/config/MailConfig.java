136357a459a01d4f0a4adf6d1897b3335a1f5486
최한윤 <chlgksdbs98@naver.com>
2023-08-08 23:37:31 +0900

    DOCS: mail info

--- server/src/main/java/com/forj/config/MailConfig.java
+++ server/src/main/java/com/forj/config/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
 		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 		
 		javaMailSender.setHost("smtp.naver.com"); // 메인 도메인 서버 주소 (smtp 서버 주소)
-		javaMailSender.setUsername("chlgksdbs98"); // 네이버 아이디
-		javaMailSender.setPassword("zxc1599611"); // 네이버 비밀번호
+		javaMailSender.setUsername("id"); // 네이버 아이디
+		javaMailSender.setPassword("password"); // 네이버 비밀번호
 		javaMailSender.setPort(465); // 메일 인증 서버 port
 		javaMailSender.setJavaMailProperties(getMailProperties()); // 메일 인증 서버 정보 가져오기
 		javaMailSender.setDefaultEncoding("UTF-8");
